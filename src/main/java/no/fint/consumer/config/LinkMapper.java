ee94ad705b60265ecf63b2b14e1f93e860139c9a
Asgeir Nilsen <asgeir@fintlabs.no>
2019-04-28 22:44:57 +0200

    Updated LinkMapper.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -2,7 +2,10 @@ package no.fint.consumer.config;
 
 import com.google.common.collect.ImmutableMap;
 import no.fint.consumer.utils.RestEndpoints;
-import no.fint.model.administrasjon.arkiv.Journalpost;
+import no.fint.model.administrasjon.arkiv.DokumentStatus;
+import no.fint.model.administrasjon.arkiv.Dokumentfil;
+import no.fint.model.administrasjon.arkiv.Saksstatus;
+import no.fint.model.administrasjon.arkiv.TilknyttetRegistreringSom;
 import no.fint.model.kultur.kulturminnevern.DispensasjonAutomatiskFredaKulturminne;
 import no.fint.model.kultur.kulturminnevern.TilskuddFartoy;
 import no.fint.model.kultur.kulturminnevern.TilskuddFredaHusPrivatEie;
@@ -16,8 +19,11 @@ public class LinkMapper {
                 .put(DispensasjonAutomatiskFredaKulturminne.class.getName(), contextPath + RestEndpoints.DISPENSASJONAUTOMATISKFREDAKULTURMINNE)
                 .put(TilskuddFartoy.class.getName(), contextPath + RestEndpoints.TILSKUDDFARTOY)
                 .put(TilskuddFredaHusPrivatEie.class.getName(), contextPath + RestEndpoints.TILSKUDDFREDAHUSPRIVATEIE)
-                .put(Journalpost.class.getName(), "/administrasjon/arkiv/journalpost")
                 /* .put(TODO,TODO) */
+                .put(Saksstatus.class.getName(), "/administrasjon/arkiv/saksstatus")
+                .put(TilknyttetRegistreringSom.class.getName(), "/administrasjon/arkiv/tilknyttetregistreringsom")
+                .put(DokumentStatus.class.getName(), "/administrasjon/arkiv/dokumentstatus")
+                .put(Dokumentfil.class.getName(), "/administrasjon/arkiv/dokumentfil")
                 .build();
     }
 

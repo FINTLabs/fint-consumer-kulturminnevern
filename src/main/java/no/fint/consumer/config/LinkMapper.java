20bfc8e6cd6957d4aa5de222f7ffce5e1d1cacbd
Asgeir Nilsen <asgeir@fintlabs.no>
2020-01-07 11:03:13 +0100

    Updated model and LinkMapper.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -14,22 +14,32 @@ import java.util.Map;
 public class LinkMapper {
 
     public static Map<String, String> linkMapper(String contextPath) {
-        return ImmutableMap.<String,String>builder()
+        return ImmutableMap.<String, String>builder()
                 .put(DispensasjonAutomatiskFredaKulturminne.class.getName(), contextPath + RestEndpoints.DISPENSASJONAUTOMATISKFREDAKULTURMINNE)
                 .put(TilskuddFartoy.class.getName(), contextPath + RestEndpoints.TILSKUDDFARTOY)
                 .put(TilskuddFredaHusPrivatEie.class.getName(), contextPath + RestEndpoints.TILSKUDDFREDAHUSPRIVATEIE)
-                /* .put(TODO,TODO) */
+
+                .put(AdministrativEnhet.class.getName(), "/administrasjon/arkiv/administrativenhet")
+                .put(Arkivdel.class.getName(), "/administrasjon/arkiv/arkivdel")
+                .put(Arkivressurs.class.getName(), "/administrasjon/arkiv/arkivressurs")
+                .put(Autorisasjon.class.getName(), "/administrasjon/arkiv/autorisasjon")
                 .put(Dokumentfil.class.getName(), "/administrasjon/arkiv/dokumentfil")
                 .put(DokumentStatus.class.getName(), "/administrasjon/arkiv/dokumentstatus")
                 .put(DokumentType.class.getName(), "/administrasjon/arkiv/dokumenttype")
                 .put(JournalpostType.class.getName(), "/administrasjon/arkiv/journalposttype")
                 .put(JournalStatus.class.getName(), "/administrasjon/arkiv/journalstatus")
+                .put(Klasse.class.getName(), "/administrasjon/arkiv/klasse")
+                .put(Klassifikasjonssystem.class.getName(), "/administrasjon/arkiv/klassifikasjonssystem")
                 .put(Korrespondansepart.class.getName(), "/administrasjon/arkiv/korrespondansepart")
                 .put(KorrespondansepartType.class.getName(), "/administrasjon/arkiv/korrespondanseparttype")
+                .put(Merknadstype.class.getName(), "/administrasjon/arkiv/merknadstype")
                 .put(Part.class.getName(), "/administrasjon/arkiv/part")
                 .put(PartRolle.class.getName(), "/administrasjon/arkiv/partrolle")
                 .put(Saksstatus.class.getName(), "/administrasjon/arkiv/saksstatus")
                 .put(TilknyttetRegistreringSom.class.getName(), "/administrasjon/arkiv/tilknyttetregistreringsom")
+                .put(Skjermingshjemmel.class.getName(), "/administrasjon/arkiv/skjermingshjemmel")
+                .put(Tilgang.class.getName(), "/administrasjon/arkiv/tilgang")
+                .put(Tilgangsrestriksjon.class.getName(), "/administrasjon/arkiv/tilgangsrestriksjon")
                 .put(Variantformat.class.getName(), "/administrasjon/arkiv/variantformat")
 
                 .put(Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement")
